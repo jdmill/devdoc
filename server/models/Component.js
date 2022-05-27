0480a6a705db913cb50e2b933c6b67ce7fa3ace4
Christian Carnevale <cncarnevale@gmail.com>
2022-05-27 14:22:49 -0400

    created models and schemas, server up and running

--- server/models/Component.js
+++ server/models/Component.js
@@ -1,10 +1,42 @@
-const {Schema, model, Types} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 // List/refence/stored info  of all available components available to make a great customized webpage
-const componentSchema = new Schema(
-    {
+const componentSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 60,
+  },
+  compType: {
+    type: String,
+    required: true,
+    validators: {
+      enum: ["header", "article-photo", "contact", "footer"],
+    },
+  },
+  links: [{
+    linkName:String, url: String}],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  lastUpdatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  imageUrl: {
+      type: String
+  },
+  text: {
+      type: String,
+      minlength: 12,
+      maxlength: 255
+  },
+  contact: {
+      type: Boolean,
+      default: false
+  }
+});
 
-
-    });
-
-export default componentSchema;
+const Component = model("Component", componentSchema);
+module.exports = Component;
