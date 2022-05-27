ffdb8dd38cb9e55c6d46549b16fee4224b63c3f5
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-27 11:59:30 -0400

    got mongodb up and running

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,39 +1,38 @@
-const {Schema, model, Types} = require('mongoose');
-
+const { Schema, model, Types } = require("mongoose");
 
 const projectSchema = new Schema(
-    {
-      projectId: {
-        type: Schema.Types.ObjectId,
-      },
-      projectTitle: {
-        type: String,
-        required: true,
-        maxlength: 280,
-      },
-      username: {
-        type: String,
-        required: true,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-      lastUpdatedAt: {
-          // ?????
-        type: Date,
-        default: Date.now,
-        // ?????
-      },
-      componentArray: {
-        type: [ componentArray] ,
-      },
-
+  {
+    projectId: {
+      type: Schema.Types.ObjectId,
     },
-    {
-      toJSON: {
-        getters: true,
-      },
-    });
+    projectTitle: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    lastUpdatedAt: {
+      // ?????
+      type: Date,
+      default: Date.now,
+      // ?????
+    },
+    componentArray: {
+      type: [componentArray],
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
-    export default projectSchema;
+// export default projectSchema;
--- server/server.js
+++ server/server.js
@@ -1,39 +1,29 @@
-const express = require('express');
-// Import the ApolloServer class
-const { ApolloServer } = require('apollo-server-express');
-// Import the two parts of a GraphQL schema
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const express = require("express");
+
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./schemas/index.js");
+const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
-await server.start();
-server.applyMiddleware({ app, path: '/graphql' });
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  })
-})
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
 };
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
-
-
-
-
-
-
-
-
-
-
-
-
