5b4a1df047f35dace9a0d8e317b611b6483056d0
Chris Rowe <rowe2ry@yahoo.com>
2022-05-27 14:40:56 -0400

    Tons of boilerplate componetry

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LandingPage from './components/landingPage/LandingPage';
+import Container from './components/container/Container';
+
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <ApolloProvider client={client}>
+      <Router>
+        <Route path="/" element={<LandingPage />}/>
+        <Route path="/app/" element={<Container />}/>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/componentPreview/ComponentPreview.js
@@ -0,0 +1,8 @@
+function ComponentPreview() {
+    return (
+        <div className="live__preview">
+            {/* TODO: figure out how to render a user component with the props of their options passed in, maybe state management on the parent component for this which is holding the editor form for the component and THIS component too */}
+        </div>
+    );
+};
+export default ComponentPreview;
