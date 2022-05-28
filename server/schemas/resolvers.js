430e76f729568cce789b05e89ffd9d7df5886927
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-28 09:17:15 -0400

    resolvers and typeDefs -still working on queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,41 +1,40 @@
-const { User } = require("../models");
+const { User, Project, Component } = require("../models");
 
 const resolvers = {
-  // Query: {
-  //   Users: async () => {
-  //     return User.find();
-  //   },
-  //   User: async (parent, { UserId }) => {
-  //     return User.findOne({ _id: UserId });
-  //   },
-  // },
-  // Mutation: {
-  //   addUser: async (parent, { name }) => {
-  //     return User.create({ name });
-  //   },
-  //   addProject: async (parent, { UserId, project }) => {
-  //     return User.findOneAndUpdate(
-  //       { _id: UserId },
-  //       {
-  //         $addToSet: { projects: project },
-  //       },
-  //       {
-  //         new: true,
-  //         runValidators: true,
-  //       }
-  //     );
-  //   },
-  //   removeUser: async (parent, { UserId }) => {
-  //     return User.findOneAndDelete({ _id: UserId });
-  //   },
-  //   removeProject: async (parent, { UserId, project }) => {
-  //     return User.findOneAndUpdate(
-  //       { _id: UserId },
-  //       { $pull: { projects: project } },
-  //       { new: true }
-  //     );
-  //   },
-  // },
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+  },
+  Mutation: {
+    // We need to change this if and when we add Auth
+    addUser: async (parent, { username, email, password }) => {
+      return User.create({ username, email, password });
+    },
+    addProject: async (parent, { projectTitle, username }) => {
+      const project = new Project({ projectTitle });
+
+      await User.findOneAndUpdate(username, {
+        $push: { projects: project },
+      });
+
+      return project;
+    },
+    // addComponent:
+    // removeUser: async (parent, { UserId }) => {
+    //   return User.findOneAndDelete({ _id: UserId });
+    // },
+    // removeProject: async (parent, { UserId, project }) => {
+    //   return User.findOneAndUpdate(
+    //     { _id: UserId },
+    //     { $pull: { projects: project } },
+    //     { new: true }
+    //   );
+    // },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,20 +3,37 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID
-    name: String
-    skills: [String]!
+    username: String
+    email: String
+    projects: [Project]
+  }
+
+  type Project {
+    _id: ID
+    username: String
+    projectTitle: String
+    componentArray: [Component]
+  }
+
+  type Component {
+    _id: ID
+    title: String
+    compType: String
   }
 
   type Query {
-    Users: [User]!
-    User(UserId: ID!): User
+    user(username: String): User
+    users: [User]
+    project(username: String): Project
+    componentArray(project: ID!): [Component]
+    component(_id: ID!): Component
   }
 
   type Mutation {
-    addUser(name: String!): User
-    addSkill(UserId: ID!, skill: String!): User
-    removeUser(UserId: ID!): User
-    removeSkill(UserId: ID!, skill: String!): User
+    addUser(username: String!, email: String!, password: String!): User
+    addProject(projectTitle: String!, username: String!): Project
+    removeUser(username: ID!): User
+    removeProject(username: ID!, projectTitle: String!): Project
   }
 `;
 
