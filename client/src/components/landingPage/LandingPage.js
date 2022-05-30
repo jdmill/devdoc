7108961253c2ccf4ea3811552b467a35f9757c29
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-30 13:26:58 -0400

    worked on landing page - almost completed mobile responsiveness of gradient

--- client/src/components/landingPage/LandingPage.js
+++ client/src/components/landingPage/LandingPage.js
@@ -1,16 +1,19 @@
-import doctor from '../../assets/image/doctor-HR.jpeg'
+import doctor from "../../assets/image/doctor-HRCustom.jpeg";
+import "./landingPage.css";
 
 function LandingPage() {
-    return (
-        <main className="splash">
-            <h1>Dev Doctor</h1>
-            <h2>A website generation tool
-            for the hard working
-            entrepreneur</h2>
-            <button>Get Started</button>
-            <img style={ { width: '50%', height:'auto' } } src={doctor} alt="Friendly cartoon doctor smiling." />
-        </main>
-    );
-};
+  return (
+    <main className="splash">
+      <img
+        className="splashImg"
+        src={doctor}
+        alt="Friendly cartoon doctor smiling."
+      />
+      <h1>Dev Doctor</h1>
+      <h2>A website generation tool for the hard working entrepreneur</h2>
+      <button>Get Started</button>
+    </main>
+  );
+}
 
 export default LandingPage;
