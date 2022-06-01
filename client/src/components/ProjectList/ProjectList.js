c089781a542ceed875026bc31e359a94ad93e9e5
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-06-01 17:30:50 -0400

    changed images in public folder

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -5,42 +5,44 @@ const ProjectList = ({ projects }) => {
   const handleEditRequest = (e) => {
     const id = e.target.name;
     console.log(`You clicked edit for project ID: ${id}`);
-};
+  };
 
-const handleDeleteRequest = (e) => {
-    const id = e.target.name
+  const handleDeleteRequest = (e) => {
+    const id = e.target.name;
     console.log(`You clicked delete for project ID: ${id}`);
-};
-  
+  };
+
   if (!projects.length) {
     return (
       <li className="list__item">
         <p className="project__title">no projects to show...</p>
       </li>
     );
-  };
+  }
 
   return (
     <div>
       {projects &&
         projects.map((project) => (
           <li key={project._id} className="list__item">
-                  <p className="project__title">{project.projectTitle}</p>
-                  <div className="project__icons">
-                      <button className="list__btns edit"
-                          name={project.id}
-                          onClick={handleEditRequest}
-                      >
-                          Edit
-                      </button>
-                      <button className="list__btns delete"
-                          name={project.id}
-                          onClick={handleDeleteRequest}
-                      >
-                          Delete
-                      </button>
-                  </div>
-              </li>
+            <p className="project__title">{project.projectTitle}</p>
+            <div className="project__icons">
+              <button
+                className="list__btns edit"
+                name={project._id}
+                onClick={handleEditRequest}
+              >
+                Edit
+              </button>
+              <button
+                className="list__btns delete"
+                name={project._id}
+                onClick={handleDeleteRequest}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
         ))}
     </div>
   );
