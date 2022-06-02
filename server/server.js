e6efdd95fcd97621e7c55a3d7f00aac9d20a1dc3
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-06-02 16:52:33 -0400

    attempting a different point

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/"));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
