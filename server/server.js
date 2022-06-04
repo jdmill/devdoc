cf9d9bd75a033a8873e98cefb90e92e74c34a542
Jon Miller <jonathan.d.mllr@gmail.com>
2022-06-03 20:12:29 -0400

    Update server.js
    moved routing

--- server/server.js
+++ server/server.js
@@ -28,6 +28,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+  
+  app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/"));
+});
 }
 
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
